debee84dea9a56b8df44bc7104fce67f120524c0
yintp <yintp@andata.cn>
2022-05-17 11:21:42 +0800

    feat[leetcode]:Q29添加两数相除

---
+++ src/test/java/com/yintp/algorithm/leetcode/Q29DivideTest.java
@@ -0,0 +1,15 @@
+package com.yintp.algorithm.leetcode;
+
+import org.junit.Test;
+
+/**
+ * @author yintp
+ */
+public class Q29DivideTest {
+    @Test
+    public void testCase1() {
+        Q29Divide q29Divide = new Q29Divide();
+        int divide = q29Divide.divide(9, -2);
+        System.out.println(divide);
+    }
+}
