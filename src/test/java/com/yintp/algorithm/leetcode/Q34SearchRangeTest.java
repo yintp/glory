1d186cd7e230f01020eba338594a813ab9998a03
yintp <yintp@andata.cn>
2022-06-13 15:49:56 +0800

    feat(leetcode):在排序数组中查找元素的第一个和最后一个

---
+++ src/test/java/com/yintp/algorithm/leetcode/Q34SearchRangeTest.java
@@ -0,0 +1,36 @@
+package com.yintp.algorithm.leetcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author yintp
+ */
+public class Q34SearchRangeTest {
+    @Test
+    public void testCase1() {
+        Q34SearchRange q34SearchRange = new Q34SearchRange();
+        int[] s = {5, 7, 7, 8, 8, 10};
+        int[] result = q34SearchRange.searchRange(s, 8);
+        int[] expect = {3, 4};
+        Assert.assertArrayEquals(expect, result);
+    }
+
+    @Test
+    public void testCase2() {
+        Q34SearchRange q34SearchRange = new Q34SearchRange();
+        int[] s = {5, 7, 7, 8, 8, 10};
+        int[] result = q34SearchRange.searchRange(s, 6);
+        int[] expect = {-1, -1};
+        Assert.assertArrayEquals(expect, result);
+    }
+
+    @Test
+    public void testCase3() {
+        Q34SearchRange q34SearchRange = new Q34SearchRange();
+        int[] s = {};
+        int[] result = q34SearchRange.searchRange(s, 0);
+        int[] expect = {-1, -1};
+        Assert.assertArrayEquals(expect, result);
+    }
+}
