dacc726f530b88391d4700035caa6fab1e5955e0
殷自豪 <zihao.yin@hairobotics.com>
2026-05-20 18:44:40 +0800

    feat(algo): topic1

--- algorithm/src/main/java/com/yintp/algorithm/leetcode/topic1/Q1TwoSum.java
+++ algorithm/src/main/java/com/yintp/algorithm/leetcode/topic1/Q1TwoSum.java
@@ -16,7 +16,14 @@ import java.util.Map;
  */
 public class Q1TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        // TODO: 哈希表存储已遍历元素，查找互补值
+        Map<Integer, Integer> map = new HashMap();
+        for (int i = 0;i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
         return new int[0];
     }
 }
--- algorithm/src/main/java/com/yintp/algorithm/leetcode/topic1/Q217ContainsDuplicate.java
+++ algorithm/src/main/java/com/yintp/algorithm/leetcode/topic1/Q217ContainsDuplicate.java
@@ -1,5 +1,8 @@
 package com.yintp.algorithm.leetcode.topic1;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * #217 存在重复元素 (Contains Duplicate) - Easy
  * 专题: 数组 & 哈希表 | 第3周 Day1
@@ -13,7 +16,13 @@ package com.yintp.algorithm.leetcode.topic1;
  */
 public class Q217ContainsDuplicate {
     public boolean containsDuplicate(int[] nums) {
-        // TODO: HashSet 去重
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            boolean add = set.add(nums[i]);
+            if (!add) {
+                return true;
+            }
+        }
         return false;
     }
 }
