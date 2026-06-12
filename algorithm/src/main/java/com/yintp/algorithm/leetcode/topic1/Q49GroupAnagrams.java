bc61f772a9ae1e8694f6b8e3fc108ebb81366203
yintp <yintengping@foxmail.com>
2026-06-12 07:02:56 +0000

    feat(algorithm): Q242、Q49

--- algorithm/src/main/java/com/yintp/algorithm/leetcode/topic1/Q49GroupAnagrams.java
+++ algorithm/src/main/java/com/yintp/algorithm/leetcode/topic1/Q49GroupAnagrams.java
@@ -18,7 +18,13 @@ import java.util.HashMap;
  */
 public class Q49GroupAnagrams {
     public List<List<String>> groupAnagrams(String[] strs) {
-        // TODO: 排序每个字符串作为key，HashMap分组
-        return new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            java.util.Arrays.sort(chars);
+            String key = new String(chars);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        }
+        return new ArrayList<>(map.values());
     }
 }
